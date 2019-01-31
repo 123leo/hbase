28f6c5f739edcb420a17a89c257edf7dbd320e3b
yaoliang <546172507@qq.com>
2019-01-31 13:45:51 +0800

    first commit

---
+++ src/main/java/com/leo/passbook/service/IUserService.java
@@ -0,0 +1,18 @@
+package com.leo.passbook.service;
+
+import com.leo.passbook.vo.Response;
+import com.leo.passbook.vo.User;
+
+/**
+ * <h1>用户服务: 创建 User 服务</h1>
+ * Created by Qinyi.
+ */
+public interface IUserService {
+
+    /**
+     * <h2>创建用户</h2>
+     * @param user {@link User}
+     * @return {@link Response}
+     * */
+    Response createUser(User user) throws Exception;
+}
---
+++ src/main/java/com/leo/passbook/vo/PassTemplateInfo.java
@@ -0,0 +1,22 @@
+package com.leo.passbook.vo;
+
+import com.leo.passbook.entity.Merchants;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <h1>优惠券模板信息</h1>
+ * Created by Qinyi.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassTemplateInfo {
+
+    /** 优惠券模板 */
+    private PassTemplate passTemplate;
+
+    /** 优惠券对应的商户 */
+    private Merchants merchants;
+}
